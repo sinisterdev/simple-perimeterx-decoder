da609060256a1bc93b704a74313c76d158d2a3d1
Tony <tonynicotra26@gmail.com>
2020-08-25 10:50:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<img src="https://avatars2.githubusercontent.com/u/18720637?s=460&u=d00bb3fa4300832dc9caf613fcff89225192466d&v=4" title="sinisterdev" alt="sinisterdev"></a>
+
+# Simple PerimeterX payload decoder
+
+
+
+### Clone
+
+- Clone this repo to your local machine using `https://github.com/sinisterdev/simple-perimeterx-decoder`
+
+### Setup
+
+- How to Setup and decode a perimeterX payload
+
+> Install all the node modules needed
+
+```shell
+$ npm i
+```
+
+> now enter your perimeterX payload in the payload.txt file, then run
+
+```shell
+$ node decode
+```
+
+- You should now have the decoded payload in the decoded directory
+
---
+++ decode.js
@@ -0,0 +1,27 @@
+const atob = require("atob");
+const fs = require("fs");
+
+function base64Process(str) {
+  return decodeURIComponent(
+    Array.prototype.map
+      .call(atob(str), function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+}
+
+function payloadDecode(message) {
+  message = base64Process(message);
+  for (var e = "", r = 0; r < message.length; r++)
+    e += String.fromCharCode(message.charCodeAt(r) ^ 50);
+  return e;
+}
+
+const payload = fs.readFileSync("payload.txt", "utf8");
+fs.writeFileSync(
+  __dirname + `/decoded/${+new Date()}.json`,
+  payloadDecode(payload)
+);
+
+console.log("Done!");
